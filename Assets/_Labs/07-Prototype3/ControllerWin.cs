d5ef88cf2a3d50eaf1413722afe54cad670867ab
HolyDarknessMystic721 <70783250+HolyDarknessMystic721@users.noreply.github.com>
2021-12-03 17:08:30 -0500

    Prototype 3 Update 1
    Before I forget to send to the Main GetHub: Created Basic terrain outline , Enemies, Collectables

---
+++ Assets/_Labs/07-Prototype3/ControllerWin.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ControllerWin : MonoBehaviour {
+    public TextMeshProUGUI scoreCounter;
+
+    private int score;
+
+    private Rigidbody rb;
+
+    public GameObject winText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        winText.SetActive(false);
+
+        score = 0;
+
+        SetCountText();
+    }
+
+    void SetCountText() {
+
+        scoreCounter.text = "Score: " + score.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Collectable")) {
+
+            other.gameObject.SetActive(false);
+
+            score = score + 100;
+            
+            SetCountText();
+        }
+    }
+}
